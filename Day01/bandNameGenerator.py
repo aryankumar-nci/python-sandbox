9b3c8a2d7572e54762624f5999bbbb463f4d71ce
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-03 11:06:22 +0100

    day 01 name generator added

--- Day01/bandNameGenerator.py
+++ Day01/bandNameGenerator.py
@@ -1,1 +1,4 @@
-print("Enter your first name: ")
+print("Welcome to the band name generator")
+city_name = input("Which city you grow: \n")
+pet_name = input("Enter your pet name: \n")
+print("Band name is : "+ city_name + " "+pet_name)
