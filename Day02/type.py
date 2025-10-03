ad79859972236f5989bc14ab35ee3dc5258f7771
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-03 11:46:49 +0100

    Day02_1

---
+++ Day02/type.py
@@ -0,0 +1,18 @@
+# length =len('1234')
+# print(length)
+#
+# print(type("Hello"))
+# print(type(123456789))
+# print(type(True))
+# print(type(3.14))
+#
+#
+# #type casting
+#
+# print(type(int("123")))
+
+user_name = input("Enter your name : ")
+length = len(user_name)
+print("Length of your name is : "+ str(length))
+
+
