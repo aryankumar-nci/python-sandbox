5762a32c0487f282a4b24c3b7fbc8943e3011f6c
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-03 11:21:45 +0100

    Day02

---
+++ Day02/pythonDataTypes.py
@@ -0,0 +1,20 @@
+#Subscripting
+print("Aryan"[-1])
+
+#"123" --> is a string not number
+
+print("123"+"321")#concatenate
+
+#Integer = Whole number
+print(123+321)
+
+#Large Integers
+print(123_456_789) #same as 123,456,789
+
+#Float Numbers
+print(3.1415)
+
+#Boolean
+print(True)
+print(False)
+
