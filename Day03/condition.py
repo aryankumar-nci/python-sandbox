4d18878902bd4e66957367d56604c37a27b40710
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-04 06:19:25 +0100

    Day03_1

--- Day03/condition.py
+++ Day03/condition.py
@@ -1,7 +1,28 @@
-print("EWelcome to the event ride....")
+print("Welcome to the event ride....")
 height = int(input("What is your height?: "))
+
+bill=0
 
 if height>120:
     print("You can ride.")
+    age=int(input("Enter your age here."))
+
+    if age<=12:
+        print("Pay $5")
+        bill=5
+    elif age<=18:
+        print("Pay $7.")
+        bill=7
+    else:
+        print("Pay $12")
+        bill=12
+    photo=input("Do you want a photo take? type y for yes and n for no.")
+    if photo =="y":
+        bill = bill +3
+
+    print(f"your final bill is {bill}")
+
+
+
 else:
-    print("you are too short for this.")    
+    print("you are too short for this.")
