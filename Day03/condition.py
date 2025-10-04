9565410b34f6a0cefda8aae128e89f447ba11d2d
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-04 05:59:56 +0100

    Day03

---
+++ Day03/condition.py
@@ -0,0 +1,7 @@
+print("EWelcome to the event ride....")
+height = int(input("What is your height?: "))
+
+if height>120:
+    print("You can ride.")
+else:
+    print("you are too short for this.")    
---
+++ Day03/operator.py
@@ -0,0 +1,7 @@
+print("Welcome to the EvenOdd_Machine -_-")
+number = int(input("Enter your number: \n"))
+
+if number%2==0:
+    print("The entered number is even.")
+else:
+    print("The entered number is odd.")
