cccb89832a004ca456cab20edda28458e1700a72
Aryan Kumar <x23310618@student.ncirl.ie>
2025-10-06 10:59:35 +0100

    day05

---
+++ Day5/loop.py
@@ -0,0 +1,6 @@
+fruits = ["apple","peach","Watermelon"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + "pie")
+print(fruits)
---
+++ Day5/sum.py
@@ -0,0 +1,16 @@
+score = [12,23,12,344,2,121,3]
+
+# print(sum(score))
+#
+# sum=0
+# for i in score:
+#     sum+=i
+# print(sum)
+print(max(score))
+
+max=0
+for i in score:
+    if i>max:
+        max=i
+print(max)
+
